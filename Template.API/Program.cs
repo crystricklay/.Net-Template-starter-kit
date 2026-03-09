9dc299a9e87ec4da69b329bd775fd89d04a12dfe
vladislavnencin <nenchin2001@gmail.com>
2026-03-09 14:59:33 +0100

    -innit

---
+++ Template.API/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Template API V1");
+        c.RoutePrefix = ""; // робить UI доступним на корені http://localhost:5277/
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
